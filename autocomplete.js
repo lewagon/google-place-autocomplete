01240c2f25d8474cc6b628fe87f4b3c4a48aaa2e
Sebastien Saunier <seb@saunier.me>
2014-11-21 12:04:48 +0100

    Booletproof code if element is not here

--- autocomplete.js
+++ autocomplete.js
@@ -1,7 +1,9 @@
 function initializeAutocomplete(id) {
   var element = document.getElementById(id);
-  var autocomplete = new google.maps.places.Autocomplete(element, { types: ['geocode'] });
-  google.maps.event.addListener(autocomplete, 'place_changed', onPlaceChanged);
+  if (element) {
+    var autocomplete = new google.maps.places.Autocomplete(element, { types: ['geocode'] });
+    google.maps.event.addListener(autocomplete, 'place_changed', onPlaceChanged);
+  }
 }
 
 function onPlaceChanged() {
